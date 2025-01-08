6d81c3715b392b1c93c5a9d22656d40358f25244
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-08 15:22:48 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
-import MyProjects from "./pages/MyProjects.jsx";
 import MySkills from "./pages/MySkills.jsx";
 import Contact from "./pages/Contact.jsx";
 import Rotlayout from "./pages/Rotlayout.jsx";
